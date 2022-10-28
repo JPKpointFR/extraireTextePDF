c554c39dbdb246f899e9cfd084e3feb9b83c47c1
Adama Cissé <adam.cis345@gmail.com>
2022-10-28 21:16:13 +0200

    extraction

---
+++ extraire_texte_pdf.py
@@ -0,0 +1,16 @@
+# EXTRAIRE LE TEXTE DES FICHIERS PDF
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+f = open("recap.pdf", "rb")
+reader = PdfFileReader(f)
+
+page0 = reader.getPage(1)
+texte = page0.extractText()
+
+texte = texte.replace("Ò", '"').replace(
+    "‘", "è").replace("‹", "à").replace("”", "é").replace("’", "ê")
+
+f.close()
+
+
+print(texte)
